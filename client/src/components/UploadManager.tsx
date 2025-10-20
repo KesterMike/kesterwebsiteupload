fe0a8db34014670a6321d9f44dce8822abfea4fc
KesterMike <88211660+KesterMike@users.noreply.github.com>
2025-10-20 02:10:57 +0100

    Remove trailing slash from apiUrl

--- client/src/components/UploadManager.tsx
+++ client/src/components/UploadManager.tsx
@@ -23,7 +23,7 @@ const UploadManager = () => {
   const [newImages, setNewImages] = useState<File[]>([]);
   const [updating, setUpdating] = useState<boolean>(false);
   const [deletingId, setDeletingId] = useState<string | null>(null);
-  const apiUrl = "https://kesterwebsiteupload-production.up.railway.app/";
+  const apiUrl = "https://kesterwebsiteupload-production.up.railway.app";
 
   const fetchUploads = async () => {
     setLoading(true);
