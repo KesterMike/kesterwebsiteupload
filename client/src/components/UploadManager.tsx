aabfdaa0abcd31e597a65c87a71fc055a7e26d73
KesterMike <88211660+KesterMike@users.noreply.github.com>
2025-11-08 08:19:42 +0100

    Update UploadManager.tsx

--- client/src/components/UploadManager.tsx
+++ client/src/components/UploadManager.tsx
@@ -98,7 +98,7 @@ const UploadManager = () => {
     data.append("category", formData.category || "");
     data.append("problem", formData.problem || "");
     data.append("solution", formData.solution || "");
-    data.append("link", formData.solution || "");
+    data.append("link", formData.link || "");
     data.append("tags", JSON.stringify(formData.tags || []));
 
     try {
